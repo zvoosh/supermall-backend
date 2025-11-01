f8c568714383d1025a6f69106175fa32f482224f
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-11-01 22:34:17 +0100

    fix:fix

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./supermall-525d6-firebase-adminsdk-fbsvc-9c2c7e400f.json");
+const serviceAccount = require("./firebase-adminsdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
