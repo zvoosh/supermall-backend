4164d3fa630fb0066c84995fc763d5d146e5f37d
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-11-02 09:48:00 +0100

    fix: fix

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
-
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
 app.post("/api/admin", async (req, res) => {
   const uid = uuidv4();
   const { fullname, username, password, email } = req.body;
